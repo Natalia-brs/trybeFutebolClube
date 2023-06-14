6ac16f525d7364bf17fdc7601fd18cf1dde33c83
natalia <ntla.brs@gmail.com>
2023-06-13 21:21:31 -0300

    fix interfaces

--- app/backend/src/database/migrations/01-create-teams.ts
+++ app/backend/src/database/migrations/01-create-teams.ts
@@ -1,5 +1,5 @@
 import { Model, QueryInterface, DataTypes } from 'sequelize';
-import Iteams from '../../interfaces/ITeams'
+import Iteams from '../../Interfaces/ITeams'
 
 export default {
   up(queryInterface: QueryInterface) {
